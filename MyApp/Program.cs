62b47f1cfd6cba7017166dd82721a65212da5e4b
Merey312 <mereidosaibek@gmail.com>
2025-10-01 21:07:24 +0500

    Add files via upload

---
+++ MyApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Log("Ошибка подключения", "ERROR");
+        Log("Успешное подключение", "INFO");
+
+        
+        int[] numbers = { -1, 0, 5, 10 };
+        foreach (int n in numbers)
+        {
+            if (n > 0)
+                Console.WriteLine($"{n} – положительное");
+            else if (n < 0)
+                Console.WriteLine($"{n} – отрицательное");
+            else
+                Console.WriteLine($"{n} – ноль");
+        }
+
+        
+        User user = new User("Merey", "merey@gmail.com");
+        user.Save();
+    }
+
+    
+    static void Log(string message, string level)
+    {
+        Console.WriteLine($"[{level}] {message}");
+    }
+}
+
+
+class User
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public void Save()
+    {
+        Console.WriteLine($"Пользователь {Name} сохранён с email {Email}");
+    }
+}
